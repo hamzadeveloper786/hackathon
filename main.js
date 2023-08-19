856ebf5158a7ebe30ff4a8746333f9a73e295077
Johnprocessor <HaCkErHaMzA@JoHnHaMzA>
2023-08-19 18:26:38 +0500

    commit

---
+++ main.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAQpjms00Fa5V0bqAMZoTaKifp3-BPPmTA",
+    authDomain: "personal-blogging-app-325a3.firebaseapp.com",
+    projectId: "personal-blogging-app-325a3",
+    storageBucket: "personal-blogging-app-325a3.appspot.com",
+    messagingSenderId: "762812688441",
+    appId: "1:762812688441:web:e443791a41bf3f407405e8"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+// show Password 
